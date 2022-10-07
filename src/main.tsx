b7ee6638dd1d7e7abe83430237833da856b7857f
josuenm <direct.josue@gmail.com>
2022-10-06 23:21:33 -0300

    feat(context provider deleted)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { ApolloProvider } from "@apollo/client";
-import { MoviesProvider } from "@contexts/MoviesContext";
 import { Dashboard, Home, Movies, SelectedMovie } from "@pages";
 import { client } from "@services/graphql/config";
 import { AnimatePresence } from "framer-motion";
@@ -12,16 +11,14 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <MoviesProvider>
-          <AnimatePresence>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/movies" element={<Movies />} />
-              <Route path="/movies/:id" element={<SelectedMovie />} />
-            </Routes>
-          </AnimatePresence>
-        </MoviesProvider>
+        <AnimatePresence>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:id" element={<SelectedMovie />} />
+          </Routes>
+        </AnimatePresence>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
