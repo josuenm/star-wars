5cb130037cffc2bdfa64fa59499fb5709b11dac0
josuenm <direct.josue@gmail.com>
2022-10-06 23:23:04 -0300

    feat(useMovies deleted)

--- src/pages/SelectedMovie.tsx
+++ src/pages/SelectedMovie.tsx
@@ -1,14 +1,9 @@
 import { Header } from "@components";
-import { useMovies } from "@contexts/MoviesContext";
 import { useLocation } from "react-router-dom";
 
 const SelectedMovie = () => {
   const location = useLocation();
   const id = location.pathname.substring(8, location.pathname.length);
-
-  const { findOne } = useMovies();
-
-  findOne(id);
 
   return (
     <main>
